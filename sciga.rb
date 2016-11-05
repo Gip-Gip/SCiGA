18721919da10c213a85e406abea18b1721f5e28c
Gip-Gip <chmithbiz@gmail.com>
2016-11-04 20:33:05 -0400

    Improved the algorithm

--- sciga.rb
+++ sciga.rb
@@ -6,90 +6,86 @@
 #
 # SCiGA initializes an array and starts at the point (r, 0), where r is the
 # given radius. It then finds the nearest value that is r away from the center
-# of the array(r, r) and isn't set to 1. It then sets that value to 1 and moves
-# to that coornate. Once the conditions can no longer be satisfied, the circle
-# is smoothed to remove any pixels that are
+# of the array(r, r) and isn't set to 1.
 #
 # SCiGA.rb implements the SCiGA algorithm and prints the circumfrence to stderr.
 # It also writes a pbm of the circle to stdout
 
 $stderr.puts "Radius?";
 
-$radius = gets.to_i + 1;
+$sqrt8 = Math.sqrt(8);
+$radius = gets.to_f;
 $diameter = $radius * 2;
-$circumfrence = 0;
-$prevaccX = $x = $radius;
-$prevaccY = $y = 0;
+$width = $diameter + 2;
+$circumfrence = 0.0;
+$xSval = ($x = $radius + 1);
+$ySval = ($y = 0);
 
-$circle = Array.new($diameter ** 2, '0');
+$circle = Array.new($width ** 2, '0');
 
 def access(x, y)
-    return $circle[y * $diameter + x - 1];
+    return $circle[y * $width + x - 1];
 end
 
 def set()
-    return $circle[$y * $diameter + $x - 1] = '1';
+    return $circle[$y * $width + $x - 1] = '1';
 end
 
 def unset()
-    return $circle[$prevaccY * $diameter + $prevaccX - 1] = '0';
+    return $circle[$prevaccY * $width + $prevaccX - 1] = '0';
 end
 
-def getLen(x, y)
-    return Math.sqrt((($radius - x).abs ** 2) + (($radius - y).abs ** 2)).round;
+def getDist(x, y)
+    return ($radius - Math.sqrt(((($radius + 1) - x).abs ** 2) + (($radius - y).abs ** 2))).abs;
 end
 
 while(1 == 1)
-    if(getLen($x - 1, $y) == $radius - 1 && access($x - 1, $y) != '1')
-        $x -= 1;
-        set();
-    elsif(getLen($x + 1, $y) == $radius - 1 && access($x + 1, $y) != '1')
-        $x += 1;
-        set();
-    elsif(getLen($x, $y - 1) == $radius - 1 && access($x, $y - 1) != '1')
-        $y -= 1;
-        set();
-    elsif(getLen($x, $y + 1) == $radius - 1 && access($x, $y + 1) != '1')
-        $y += 1;
-        set();
-    elsif(getLen($x - 1, $y - 1) == $radius - 1 && access($x - 1, $y - 1) != '1')
-        $x -= 1;
-        $y -= 1;
-        set();
-    elsif(getLen($x + 1, $y - 1) == $radius - 1 && access($x + 1, $y - 1) != '1')
-        $x += 1;
-        $y -= 1;
-        set();
-    elsif(getLen($x - 1, $y + 1) == $radius - 1 && access($x - 1, $y + 1) != '1')
-        $x -= 1;
-        $y += 1;
-        set();
-    elsif(getLen($x + 1, $y + 1) == $radius - 1 && access($x + 1, $y + 1) != '1')
-        $x += 1;
-        $y += 1;
+    $dist = 0.5;
+
+    if(getDist($x - 1, $y) < $dist && access($x - 1, $y) == '0')
+        $dist = getDist($xSval = $x - 1, $ySval = $y);
+    end
+
+    if(getDist($x + 1, $y) < $dist && access($x + 1, $y) == '0')
+        $dist = getDist($xSval = $x + 1, $ySval = $y);
+    end
+
+    if(getDist($x, $y - 1) < $dist && access($x, $y - 1) == '0')
+        $dist = getDist($xSval = $x, $ySval = $y - 1);
+    end
+
+    if(getDist($x - 1, $y - 1) < $dist && access($x - 1, $y - 1) == '0')
+        $dist = getDist($xSval = $x - 1, $ySval = $y - 1);
+    end
+
+    if(getDist($x + 1, $y - 1) < $dist && access($x + 1, $y - 1) == '0')
+        $dist = getDist($xSval = $x + 1, $ySval = $y - 1);
+    end
+
+    if(getDist($x, $y + 1) < $dist && access($x, $y + 1) == '0')
+        $dist = getDist($xSval = $x, $ySval = $y + 1);
+    end
+
+    if(getDist($x - 1, $y + 1) < $dist && access($x - 1, $y + 1) == '0')
+        $dist = getDist($xSval = $x - 1, $ySval = $y + 1);
+    end
+
+    if(getDist($x + 1, $y + 1) < $dist && access($x + 1, $y + 1) == '0')
+        $dist = getDist($xSval = $x + 1, $ySval = $y + 1);
+    end
+
+    if(access($xSval, $ySval) == '0')
+        $circumfrence += Math.sqrt(($xSval - $x).abs + ($ySval - $y).abs ** 2);
+        $x = $xSval;
+        $y = $ySval;
+
         set();
     else
-        i = 0;
 
-        $stderr.puts "Smoothing out the circle...";
+        $stderr.puts "The circumfrence is #{$circumfrence} pixels in length. \n PI is equal to #{$circumfrence / $diameter}";
 
-        while(i < $circle.length)
-            if(i > $radius && ($circle[i - 1]  == '1' || $circle[i + 1] == '1') && ($circle[i - $diameter] == '1' || $circle[i + $diameter] == '1') && $circle[i] == '1')
-                $circle[i] = '0';
-                $circumfrence -= 1;
-            end
-            i += 1;
-        end
-
-        $stderr.puts "The circumfrence is #{$circumfrence} pixels in length";
-
-        $stdout.puts "P1\n#{$diameter} #{$diameter}\n#{$circle.join}"
+        $stdout.puts "P1\n#{$width.to_i} #{$width.to_i}\n#{$circle.join}"
 
         exit;
     end
-
-    $prevaccX = $x;
-    $prevaccY = $y;
-
-    $circumfrence += 1;
 end
